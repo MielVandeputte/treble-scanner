44e89e1eccdbed84e3a7e05ac5371cbe65ca5376
Miel Vandeputte <Vandeputte.miel@gmail.com>
2023-09-09 16:58:28 +0200

    border

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import clsx from 'clsx';
 const supabase = createClient<Database>(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY, {auth: { autoRefreshToken: false, persistSession: false, detectSessionInUrl: false}});
 
 export default function App() {
-
     let active = true;
     const [result, setResult] = useState<string>('');
 
@@ -35,7 +34,7 @@ export default function App() {
                 <QrReader onResult={handleScan} constraints={{facingMode: 'environment'}} className='w-full'/>
             </main>
 
-            <aside className={clsx('fixed overflow-hidden z-50 transition duration-200 w-full h-1/3 bg-opacity-95 bottom-0 flex items-center p-5', result == 'success' && 'bg-green-800', result == 'alreadyScanned' && 'bg-yellow-800', result == 'noTicket' && 'bg-red-800', result == '' && 'bg-zinc-900')}>
+            <aside className={clsx('fixed border-t-2 overflow-hidden z-50 transition duration-200 w-full h-1/3 bg-opacity-95 bottom-0 flex items-center p-5', result == 'success' && 'bg-green-800 border-green-900', result == 'alreadyScanned' && 'bg-yellow-800 border-yellow-900', result == 'noTicket' && 'bg-red-800 border-red-900', result == '' && 'bg-zinc-900 border-zinc-950')}>
                 <h1 className={clsx('absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-center text-white font-sans font-bold text-7xl', result == 'success'? 'fade-in': 'fade-out')}>Success</h1>
                 <h1 className={clsx('absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-center text-white font-sans font-bold text-7xl', result == 'noTicket'? 'fade-in': 'fade-out')}>Geen ticket</h1>
                 <h1 className={clsx('absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-center text-white font-sans font-bold text-7xl', result == 'alreadyScanned'? 'fade-in': 'fade-out')}>Al gescand</h1>
