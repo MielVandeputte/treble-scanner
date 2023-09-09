1b3f920657642e620a46526794dc8b47888699c8
Miel Vandeputte <Vandeputte.miel@gmail.com>
2023-09-09 13:58:22 +0200

    supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import { QrReader } from "react-qr-reader";
+import { QrReader } from 'react-qr-reader';
+//import { createClient } from '@supabase/supabase-js';
+//import { Database } from '../types/supabase.ts';
+
+//const supabase = createClient<Database>(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY, {auth: { autoRefreshToken: false, persistSession: false, detectSessionInUrl: false}});
 
 export default function App() {
 
     const handleScan = (result: any) => {
         if (result) {
-            console.log(result.text)
+            //const test = supabase.rpc('scanTicket', {'eventIdArg': 'minimaxi2024', 'secretCodeArg': result.text});
+
+            console.log('tset')
+            //console.log(test)
         }
     }
 
-
     return (
-        <>
-            <QrReader onResult={handleScan} constraints={ {facingMode: 'user'} }        
-            />
-        </>
+        <div className='w-full bg-zinc-950'>
+            <QrReader onResult={handleScan} constraints={ {facingMode: { exact: 'environment' }} } className='w-full h-full' />
+        </div>
     );
 }
