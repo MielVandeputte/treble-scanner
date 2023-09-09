4aadf8bc04966b9e4199028195e3d31fa1289325
Miel Vandeputte <Vandeputte.miel@gmail.com>
2023-09-09 11:49:39 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
     plugins: [
         react(),
         VitePWA({ 
-            registerType: 'autoUpdate',
             manifest: {
                 name: 'Glow Scanner',
                 short_name: 'Glow Scanner',
