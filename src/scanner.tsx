d044fbc55416d44f058a2b77bb12a15bc7cfe85f
Miel Vandeputte <Vandeputte.miel@gmail.com>
2023-09-09 12:35:27 +0200

    custom test

--- src/scanner.tsx
+++ src/scanner.tsx
@@ -1,49 +1,22 @@
 // @ts-nocheck
 
-import { Html5QrcodeScanner } from 'html5-qrcode';
+import { Html5Qrcode } from 'html5-qrcode';
 import { useEffect } from 'react';
-
-const qrcodeRegionId = 'html5qr-code-full-region';
-
-// Creates the configuration object for Html5QrcodeScanner.
-const createConfig = (props) => {
-    let config = {};
-    if (props.fps) {
-        config.fps = props.fps;
-    }
-    if (props.qrbox) {
-        config.qrbox = props.qrbox;
-    }
-    if (props.aspectRatio) {
-        config.aspectRatio = props.aspectRatio;
-    }
-    if (props.disableFlip !== undefined) {
-        config.disableFlip = props.disableFlip;
-    }
-    return config;
-};
 
 export default function Scanner(props) {
     useEffect(() => {
-        // when component mounts
-        const config = createConfig(props);
-        const verbose = props.verbose === true;
-        // Suceess callback is required.
-        if (!(props.qrCodeSuccessCallback)) {
-            throw 'qrCodeSuccessCallback is required callback.';
-        }
-        const html5QrcodeScanner = new Html5QrcodeScanner(qrcodeRegionId, config, verbose);
-        html5QrcodeScanner.render(props.qrCodeSuccessCallback, props.qrCodeErrorCallback);
-
-        // cleanup function when component will unmount
-        return () => {
-            html5QrcodeScanner.clear().catch(error => {
-                console.error('Failed to clear html5QrcodeScanner. ', error);
-            });
+        const html5QrCode = new Html5Qrcode('reader', true);
+        const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+            console.log('success')
         };
+
+        const config = { fps: 30, qrbox: { width: 250, height: 250 }};
+
+        html5QrCode.start({ facingMode: 'environment' }, config, qrCodeSuccessCallback);
+
     }, []);
 
     return (
-        <div id={qrcodeRegionId} />
+        <div id='reader' className='w-full' />
     );
 }
