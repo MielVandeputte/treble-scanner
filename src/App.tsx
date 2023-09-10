38e5df893c9fd547f406a438f17583501e0b5ba8
Miel Vandeputte <Vandeputte.miel@gmail.com>
2023-09-10 18:01:07 +0200

    hide button

--- src/App.tsx
+++ src/App.tsx
@@ -103,9 +103,10 @@ export default function App() {
 
             <header className={clsx('fixed border-t-2 overflow-hidden z-50 transition duration-200 w-full h-1/3 bg-opacity-95 bottom-0 p-5', result == 'success' && 'bg-green-800 border-green-900', result == 'alreadyScanned' && 'bg-yellow-800 border-yellow-900', result == 'noTicket' && 'bg-red-800 border-red-900', result == '' && 'bg-zinc-900 border-zinc-950')}>
                 <section className='flex w-full justify-around'>
-                    <button className='' onClick={toggleFlash}>
-                        {
-                            hasFlash?
+
+                    {
+                        hasFlash?            
+                            <button className='' onClick={toggleFlash}>
                                 isFlashOn?
                                     <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='#999999' className='w-6 h-6'>
                                         <path fill-rule='evenodd' d='M14.615 1.595a.75.75 0 01.359.852L12.982 9.75h7.268a.75.75 0 01.548 1.262l-10.5 11.25a.75.75 0 01-1.272-.71l1.992-7.302H3.75a.75.75 0 01-.548-1.262l10.5-11.25a.75.75 0 01.913-.143z' clip-rule='evenodd' />
@@ -113,20 +114,21 @@ export default function App() {
 
                                     <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='#999999' className='w-6 h-6'>
                                         <path d='M20.798 11.012l-3.188 3.416L9.462 6.28l4.24-4.542a.75.75 0 011.272.71L12.982 9.75h7.268a.75.75 0 01.548 1.262zM3.202 12.988L6.39 9.572l8.148 8.148-4.24 4.542a.75.75 0 01-1.272-.71l1.992-7.302H3.75a.75.75 0 01-.548-1.262zM3.53 2.47a.75.75 0 00-1.06 1.06l18 18a.75.75 0 101.06-1.06l-18-18z' />
-                                    </svg>:
-                                <></>
-                        }
-                    </button>
+                                    </svg>
+                            </button>:
+                        <></>
+                    }
 
-                    <button className='' onClick={toggleCamera} disabled={switchingCameras}>
-                        {
-                            listCameras && listCameras.length > 1?
+                    {
+                        listCameras && listCameras.length > 1?
+                            <button className='' onClick={toggleCamera} disabled={switchingCameras}>
                                 <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='#999999' className='w-6 h-6'>
                                     <path fillRule='evenodd' d='M4.755 10.059a7.5 7.5 0 0112.548-3.364l1.903 1.903h-3.183a.75.75 0 100 1.5h4.992a.75.75 0 00.75-.75V4.356a.75.75 0 00-1.5 0v3.18l-1.9-1.9A9 9 0 003.306 9.67a.75.75 0 101.45.388zm15.408 3.352a.75.75 0 00-.919.53 7.5 7.5 0 01-12.548 3.364l-1.902-1.903h3.183a.75.75 0 000-1.5H2.984a.75.75 0 00-.75.75v4.992a.75.75 0 001.5 0v-3.18l1.9 1.9a9 9 0 0015.059-4.035.75.75 0 00-.53-.918z' clipRule='evenodd' />
-                                </svg>:
-                            <></>
-                        }
-                    </button>
+                                </svg>
+                            </button>:
+                        <></>
+                    }
+                    
                 </section>
                 <section className='flex items-center w-full'>
                     <h1 className={clsx('absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-center text-white font-sans font-bold text-7xl', result == 'success'? 'fade-in': 'fade-out')}>Success</h1>
