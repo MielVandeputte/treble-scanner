b946050d5963498d980a32ddbc200eaae8b9d8ae
Miel Vandeputte <Vandeputte.miel@gmail.com>
2023-09-12 20:46:25 +0200

    bottom bar

--- src/scanner.tsx
+++ src/scanner.tsx
@@ -26,12 +26,12 @@ export default function Scanner() {
     const [environmentState, setEnvironmentState] = useState<boolean>(true);
     const [switchingCameras, setSwitchingCameras] = useState<boolean>(false);
     
-    const [qr, setQr] = useState<string>('');
+    const [qr, setQr] = useState<string>('jkgjkwjtpoiuiopipoir');
     const [code, setCode] = useState<string>('');
-    const [ownerName, setOwnerName] = useState<string>('');
-    const [ownerEmail, setOwnerEmail] = useState<string>('');
+    const [ownerName, setOwnerName] = useState<string>('Miel Vandeputte');
+    const [ownerEmail, setOwnerEmail] = useState<string>('vandeputte.miel@gmail.com');
     const [ticketTypeId, setTicketTypeId] = useState<number>(0);
-    const [ticketTypeName, setTicketTypeName] = useState<string>('');
+    const [ticketTypeName, setTicketTypeName] = useState<string>('Voorverkoop');
 
     const historyContext = useContext(HistoryContext);
     const scanSessionContext = useContext(ScanSessionContext);
@@ -138,10 +138,10 @@ export default function Scanner() {
             <div id='overlay' className={clsx('border-[8px] border-solid rounded-md border-opacity-40 transition duration-200', code == 'success' && 'border-green-800', code == 'alreadyScanned' && 'border-yellow-800', code == 'noTicket' && 'border-red-800', code == '' && 'border-zinc-200')}/>
 
             <header className={clsx('absolute overflow-hidden z-50 transition duration-200 w-full h-1/3 bg-opacity-95 bottom-0 p-5', code == 'success' && 'bg-green-800', code == 'alreadyScanned' && 'bg-yellow-800', code == 'noTicket' && 'bg-red-800', code == '' && 'bg-zinc-900')}>
-                <section className='flex w-full justify-around no-blue-box'>
+                <section className='flex w-full justify-around no-blue-box h-1/5'>
                     {
                         hasFlash?            
-                            <button className='' onClick={toggleFlash}>
+                            <button className='w-12 aspect-square flex justify-center items-center' onClick={toggleFlash}>
                                 {
                                     isFlashOn?
                                         <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill={code == ''? '#999999': '#ffffff'} className='w-6 h-6'>
@@ -158,7 +158,7 @@ export default function Scanner() {
 
                     {
                         listCameras && listCameras.length > 1?
-                            <button className='' onClick={toggleCamera} disabled={switchingCameras}>
+                            <button className='w-12 aspect-square flex justify-center items-center' onClick={toggleCamera} disabled={switchingCameras}>
                                 <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill={code == ''? '#999999': '#ffffff'} className='w-6 h-6'>
                                     <path fillRule='evenodd' d='M4.755 10.059a7.5 7.5 0 0112.548-3.364l1.903 1.903h-3.183a.75.75 0 100 1.5h4.992a.75.75 0 00.75-.75V4.356a.75.75 0 00-1.5 0v3.18l-1.9-1.9A9 9 0 003.306 9.67a.75.75 0 101.45.388zm15.408 3.352a.75.75 0 00-.919.53 7.5 7.5 0 01-12.548 3.364l-1.902-1.903h3.183a.75.75 0 000-1.5H2.984a.75.75 0 00-.75.75v4.992a.75.75 0 001.5 0v-3.18l1.9 1.9a9 9 0 0015.059-4.035.75.75 0 00-.53-.918z' clipRule='evenodd' />
                                 </svg>
@@ -166,32 +166,31 @@ export default function Scanner() {
                         <></>
                     }
 
-                    <Link to={'/menu'}>
+                    <Link to={'/menu'} className='w-12 aspect-square flex justify-center items-center '>
                         <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill={code == ''? '#999999': '#ffffff'} className='w-6 h-6'>
                             <path fillRule='evenodd' d='M10.5 6a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zm0 6a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zm0 6a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z' clipRule='evenodd' />
                         </svg>
                     </Link>
                 </section>
                     
-                <section className='w-full h-full'>
-                    <h1 className={clsx('absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-center text-white font-sans font-bold text-7xl', code == 'success'? 'fade-in': 'fade-out')}>Success</h1>
-                    <h1 className={clsx('absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-center text-white font-sans font-bold text-7xl', code == 'noTicket'? 'fade-in': 'fade-out')}>Geen ticket</h1>
-                    <h1 className={clsx('absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-center text-white font-sans font-bold text-7xl', code == 'alreadyScanned'? 'fade-in': 'fade-out')}>Al gescand</h1>
-                    <h1 className={clsx('absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-center text-zinc-400 logo text-7xl', code == ''? 'fade-in': 'fade-out')}>glow</h1>
+                <section className='w-full relative h-3/5'>
+                    <h1 className={clsx('absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-[55%] text-center text-white font-sans font-bold text-7xl', code == 'success'? 'fade-in': 'fade-out')}>
+                        Success
+                    </h1>
+                    <h1 className={clsx('absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-[55%] text-center text-white font-sans font-bold text-7xl', code == 'noTicket'? 'fade-in': 'fade-out')}>
+                        Geen<br/>ticket
+                    </h1>
+                    <h1 className={clsx('absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-[55%] text-center text-white font-sans font-bold text-7xl', code == 'alreadyScanned'? 'fade-in': 'fade-out')}>
+                        Al<br/>gescand
+                    </h1>
+                    <h1 className={clsx('absolute left-1/2 -translate-x-1/2 bottom-1/2 translate-y-[55%]  text-center text-zinc-400 logo text-7xl', code == ''? 'fade-in': 'fade-out')}>
+                        glow
+                    </h1>
                 </section>
 
-                <section className={clsx('absolute bottom-0 w-full transition duration-200',!code && 'hidden')}>
-                    <div className='text-center'>
-                        <span className='text-white font-sans text-xl font-semibold'>QR: {qr}</span>
-                    </div>
-                    <div className='text-center'>
-                        <span className='text-white font-sans text-xl font-semibold mr-10'>Id Ticket: {ticketTypeId}</span>
-                        <span className='text-white font-sans text-xl font-semibold'>Naam Ticket: {ticketTypeName}</span>
-                    </div>
-                    <div className='text-center'>
-                        <span className='text-white font-sans text-xl font-semibold mr-10'>Naam: {ownerName}</span>
-                        <span className='text-white font-sans text-xl font-semibold'>Email: {ownerEmail}</span>
-                    </div>
+                <section className={clsx('h-1/5 w-full transition duration-200',!code && 'hidden')}>                    
+                    <div className='text-white font-sans text-xl text-center font-semibold'>Type {ticketTypeId} | {ticketTypeName}</div>
+                    <div className='text-white font-sans text-xl text-center font-semibold'>{ownerName} | {ownerEmail}</div>
                 </section>
             </header>
         </main>
