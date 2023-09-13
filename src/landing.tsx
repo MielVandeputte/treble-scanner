336bd1f64496f85e66f4f29bfa73200d33ab7513
Miel Vandeputte <Vandeputte.miel@gmail.com>
2023-09-13 14:58:33 +0200

    landing UI update in line with scanner

--- src/landing.tsx
+++ src/landing.tsx
@@ -49,29 +49,29 @@ export default function Landing() {
         <div className='bg-zinc-900 w-screen h-screen flex justify-center items-center p-5 select-none'>            
             <main className='flex justify-center items-center'>
                 <div className='flex flex-col items-center'>
-                    <h1 className='text-center text-white logo text-7xl'>glow</h1>
+                    <h1 className='text-center text-white logo text-5xl'>glow</h1>
 
-                    <p className='text-zinc-200 my-10 text-xl text-center max-w-[600px]'>
+                    <p className='text-zinc-200 my-10 font-semibold text-center max-w-[600px]'>
                         Voer de event-id en de geheime code in om te beginnen scannen.
                         Beide zijn te vinden in het dasboard op glow-events.be.
                     </p>
 
                     <form onSubmit={handleSubmit} className='w-full sm:px-16'>
                         <div className='flex items-center'>
-                            <label className='text-zinc-200 text-xl font-semibold w-32'>
+                            <label className='text-zinc-200 font-semibold w-24'>
                                 event-id
                             </label>
-                            <input type='text' onChange={(event) => {setEventId(event.target.value)}} id='eventId' name='eventId' autoComplete='off' maxLength={50} required className='py-3 px-5 w-full text-zinc-200 rounded-full bg-zinc-800 text-xl'/>
+                            <input type='text' onChange={(event) => {setEventId(event.target.value)}} id='eventId' name='eventId' autoComplete='off' maxLength={50} required className='py-3 px-5 w-full text-zinc-200 rounded-full bg-zinc-800'/>
                         </div>
 
                         <div className='flex items-center mt-5'>
-                            <label className='text-zinc-200 text-xl font-semibold w-32'>
+                            <label className='text-zinc-200 font-semibold w-24'>
                                 code
                             </label>
-                            <input type='text' onChange={(event) => {setEventscanAuthorizationCode(event.target.value)}} id='eventscanAuthorizationCode' name='eventscanAuthorizationCode' autoComplete='off' maxLength={50} required className='py-3 px-5 w-full text-zinc-200 rounded-full bg-zinc-800 text-xl'/>
+                            <input type='text' onChange={(event) => {setEventscanAuthorizationCode(event.target.value)}} id='eventscanAuthorizationCode' name='eventscanAuthorizationCode' autoComplete='off' maxLength={50} required className='py-3 px-5 w-full text-zinc-200 rounded-full bg-zinc-800'/>
                         </div>
 
-                        <button type='submit' disabled={eventId === '' || eventscanAuthorizationCode === ''} className={clsx(loading && 'pointer-events-none animate-pulse', !loading && 'disabled:bg-transparent disabled:border-zinc-800 disabled:text-zinc-400', 'bg-emerald-800 mt-10 border-2 border-transparent rounded-full whitespace-nowrap transition duration-200 text-white select-none h-14 w-full text-center font-semibold text-xl no-blue-box')}>
+                        <button type='submit' disabled={eventId === '' || eventscanAuthorizationCode === ''} className={clsx(loading && 'pointer-events-none animate-pulse', !loading && 'disabled:bg-transparent disabled:border-zinc-800 disabled:text-zinc-400', 'bg-emerald-800 mt-10 border-2 border-transparent rounded-full whitespace-nowrap transition duration-200 text-white select-none h-12 w-full text-center font-semibold no-blue-box')}>
                             Start
                         </button>
                     </form>
