74929970668d35ded5a8d3aca4920138783bfbec
Miel Vandeputte <Vandeputte.miel@gmail.com>
2023-09-13 18:10:45 +0200

    iconstest

--- vite.config.ts
+++ vite.config.ts
@@ -33,6 +33,12 @@ export default defineConfig({
                         src: 'maskable-icon-512x512.png',
                         sizes: '512x512',
                         type: 'image/png',
+                        purpose: 'maskable'
+                    },
+                    {
+                        src: 'maskable-icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
                         purpose: 'any'
                     }
                 ]
