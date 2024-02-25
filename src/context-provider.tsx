480c3bac20637c1e7f516880368a68b5334fdf75
MielVandeputte <vandeputte.miel@gmail.com>
2024-02-25 09:38:27 +0100

    feat(landscape-warning)

--- src/context-provider.tsx
+++ src/context-provider.tsx
@@ -47,10 +47,14 @@ export function ContextProvider(props: Readonly<{ children: ReactNode }>) {
     );
     const [ticketScanAttemptHistoryState, setTicketScanAttemptHistoryState] = useState<TicketScanAttempt[]>([]);
     const [internetConnectedState, setInternetConnectedState] = useState<boolean>(navigator.onLine);
+    const [portraitOrientationState, setPortraitOrientationState] = useState<boolean>(
+        window.screen.orientation.type === 'portrait-primary' || window.screen.orientation.type === 'portrait-secondary'
+    );
 
     useEffect(() => {
         window.addEventListener('online', refreshInternetConnectedState);
         window.addEventListener('offline', refreshInternetConnectedState);
+        window.addEventListener('resize', refreshOrientationState);
 
         const serializedTicketScanAttemptHistory = store.get('ticketScanAttemptHistory');
 
@@ -73,6 +77,13 @@ export function ContextProvider(props: Readonly<{ children: ReactNode }>) {
         setInternetConnectedState(navigator.onLine);
     }
 
+    function refreshOrientationState(): void {
+        setPortraitOrientationState(
+            window.screen.orientation.type === 'portrait-primary' ||
+                window.screen.orientation.type === 'portrait-secondary'
+        );
+    }
+
     function addTicketScanAttemptToHistory(ticketScanAttempt: TicketScanAttempt): void {
         setTicketScanAttemptHistoryState((prevHistory) => {
             const ticketScanAttemptHistory = sortTicketScanAttemptHistoryByTimestamp([
@@ -90,23 +101,45 @@ export function ContextProvider(props: Readonly<{ children: ReactNode }>) {
     }
 
     return (
-        <InternetConnectedContext.Provider value={internetConnectedState}>
-            <ScannerCredentialsContext.Provider
-                value={{
-                    scannerCredentials: scannerCredentialsState,
-                    setScannerCredentials: setScannerCredentialsState,
-                }}
-            >
-                <TicketScanAttemptHistoryContext.Provider
-                    value={{
-                        ticketScanAttemptHistory: ticketScanAttemptHistoryState,
-                        addTicketScanAttemptToHistory: addTicketScanAttemptToHistory,
-                        clearTicketScanAttemptHistory: clearTicketHistory,
-                    }}
-                >
-                    {props.children}
-                </TicketScanAttemptHistoryContext.Provider>
-            </ScannerCredentialsContext.Provider>
-        </InternetConnectedContext.Provider>
+        <>
+            {portraitOrientationState ? (
+                <InternetConnectedContext.Provider value={internetConnectedState}>
+                    <ScannerCredentialsContext.Provider
+                        value={{
+                            scannerCredentials: scannerCredentialsState,
+                            setScannerCredentials: setScannerCredentialsState,
+                        }}
+                    >
+                        <TicketScanAttemptHistoryContext.Provider
+                            value={{
+                                ticketScanAttemptHistory: ticketScanAttemptHistoryState,
+                                addTicketScanAttemptToHistory: addTicketScanAttemptToHistory,
+                                clearTicketScanAttemptHistory: clearTicketHistory,
+                            }}
+                        >
+                            {props.children}
+                        </TicketScanAttemptHistoryContext.Provider>
+                    </ScannerCredentialsContext.Provider>
+                </InternetConnectedContext.Provider>
+            ) : (
+                <div className="w-screen h-dvh flex justify-center items-center flex-col transition duration-200 select-none">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="#999999"
+                        className="w-6 h-6 mb-6 mx-6"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+                        />
+                    </svg>
+                    <h1 className="font-semibold text-center text-zinc-400">Draai je apparaat</h1>
+                </div>
+            )}
+        </>
     );
 }
