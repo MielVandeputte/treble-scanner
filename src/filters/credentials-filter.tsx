9913c784257d24c97ec8a377ebedd90ef5fec471
Miel Vandeputte <vandeputte.miel@gmail.com>
2025-04-21 23:11:23 +0200

    refactor: remove use effect

--- src/filters/credentials-filter.tsx
+++ src/filters/credentials-filter.tsx
@@ -1,5 +1,5 @@
-import { JSX, ReactNode, use, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { JSX, ReactNode, use } from 'react';
+import { Navigate } from 'react-router-dom';
 
 import { ScanCredentialsContext } from '../contexts/scan-credentials-context.tsx';
 import { LOGIN_PATH, SCANNER_PATH } from '../main.tsx';
@@ -13,27 +13,18 @@ export function CredentialsFilter({
   assertNotPresent?: boolean;
   children: ReactNode;
 }): JSX.Element | ReactNode | null {
-  const navigate = useNavigate();
   const scanCredentials = use(ScanCredentialsContext).scanCredentials;
-
-  useEffect(() => {
-    if (assertPresent && scanCredentials === null) {
-      navigate(LOGIN_PATH, { replace: true, viewTransition: true });
-    } else if (assertNotPresent && scanCredentials !== null) {
-      navigate(SCANNER_PATH, { replace: true, viewTransition: true });
-    }
-  }, [navigate, scanCredentials, assertPresent, assertNotPresent]);
 
   if (scanCredentials === undefined) {
     return null;
   }
 
   if (assertPresent && scanCredentials === null) {
-    return null;
+    return <Navigate to={LOGIN_PATH} />;
   }
 
   if (assertNotPresent && scanCredentials !== null) {
-    return null;
+    return <Navigate to={SCANNER_PATH} />;
   }
 
   return children;
