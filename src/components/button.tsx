3cad993318f94dec91f24c627cd32ebc54ef0ce8
Miel Vandeputte <vandeputte.miel@gmail.com>
2026-01-03 10:10:06 +0100

    refactor: replace forward ref with new ref passing way

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,24 +1,26 @@
 import { clsx } from 'clsx';
-import { ButtonHTMLAttributes, forwardRef } from 'react';
+import { ButtonHTMLAttributes, JSX, Ref } from 'react';
 
-type ButtonProps = Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'ref' | 'disabled' | 'className' | 'aria-busy'> & {
+type ButtonProps = Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'disabled' | 'className' | 'aria-busy'> & {
   loading?: boolean;
+  ref?: Ref<HTMLButtonElement>;
 };
 
-export const Button = forwardRef<HTMLButtonElement, Readonly<ButtonProps>>(
-  ({ loading = false, children, ...props }, ref) => (
+export function Button({ loading = false, children, ref, ...props }: Readonly<ButtonProps>): JSX.Element {
+  return (
     <button
       {...props}
       ref={ref}
       disabled={loading}
       className={clsx(
-        loading ? 'animate-pulse' : '',
+        {
+          'animate-pulse': loading,
+        },
         'flex h-11 items-center justify-center rounded-full bg-emerald-900 px-12 text-center font-semibold whitespace-nowrap text-zinc-200 outline-0 outline-transparent transition select-none active:scale-95 active:bg-emerald-800 active:text-white',
       )}
       aria-busy={loading}
     >
       {children}
     </button>
-  ),
-);
-Button.displayName = 'Button';
+  );
+}
--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,25 +1,27 @@
 import { clsx } from 'clsx';
-import { InputHTMLAttributes, forwardRef } from 'react';
+import { InputHTMLAttributes, Ref, JSX } from 'react';
 
-type InputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'ref' | 'className' | 'aria-invalid'> & {
+type InputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'className' | 'aria-invalid'> & {
   label: string;
   invalid?: boolean;
+  ref?: Ref<HTMLInputElement>;
 };
 
-export const Input = forwardRef<HTMLInputElement, Readonly<InputProps>>(({ label, invalid = false, ...props }, ref) => (
-  <div className="w-full">
-    <label htmlFor={props.id} className="block px-2 pb-1 font-semibold select-none">
-      {label}
-    </label>
-    <input
-      {...props}
-      ref={ref}
-      className={clsx(
-        invalid ? 'outline-rose-950 focus:outline-rose-800' : 'outline-transparent focus:outline-zinc-400',
-        'w-full rounded-full bg-zinc-800 px-5 py-2 font-semibold text-zinc-200 outline-2 transition',
-      )}
-      aria-invalid={invalid}
-    />
-  </div>
-));
-Input.displayName = 'Input';
+export function Input({ label, invalid = false, ref, ...props }: Readonly<InputProps>): JSX.Element {
+  return (
+    <div className="w-full">
+      <label htmlFor={props.id} className="block px-2 pb-1 font-semibold select-none">
+        {label}
+      </label>
+      <input
+        {...props}
+        ref={ref}
+        className={clsx(
+          invalid ? 'outline-rose-950 focus:outline-rose-800' : 'outline-transparent focus:outline-zinc-400',
+          'w-full rounded-full bg-zinc-800 px-5 py-2 font-semibold text-zinc-200 outline-2 transition',
+        )}
+        aria-invalid={invalid}
+      />
+    </div>
+  );
+}
